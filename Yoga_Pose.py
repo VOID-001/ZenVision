2f351f69b47e1fd00e3c4cd11165feb4d4bc303e
BooTheCreator <boosolini123@gmail.com>
2024-04-03 13:05:43 +0530

    Added Working Camera

--- Yoga_Pose.py
+++ Yoga_Pose.py
@@ -1,3 +1,15 @@
-import cv2
+import cv2 as cv #Computer Vision
+import matplotlib.pyplot as plt #Pyplot for pose detection
+capture = cv.VideoCapture(0)
+while(True):
+    _,frame= capture.read()
+    cv.imshow('livestream',frame)
 
-print(cv2.__version__)
+    if cv.waitKey(1) == ord("q"):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+    
+#print(cv.__version__)
+
