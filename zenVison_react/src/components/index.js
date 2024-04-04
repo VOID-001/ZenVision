6fb7aa9de4a5fcae2d19cfbca569593d1f014b94
VOID <95576296+VOID-001@users.noreply.github.com>
2024-04-04 11:07:56 +0530

    Test

--- zenVison_react/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Login } from "./auth/login/login";
-export { default as Register } from "./auth/register/register";
-export { default as Ai } from "./ai/Ai";
-// export { default as Navbar } from "./landing_page/Navbar";
-// export { default as Footer } from "./landing_page/Footer.jsx";
